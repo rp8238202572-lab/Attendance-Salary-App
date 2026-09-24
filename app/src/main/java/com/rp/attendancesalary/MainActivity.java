e340b4adbafe3f2f0063f2ec2bbb540559e11db3
rp8238202572-lab <rp8238202572@gmail.com>
2026-09-24 11:56:51 +0530

    Prepare Android Play Store project: app/src/main/java/com/rp/attendancesalary/MainActivity.java

---
+++ app/src/main/java/com/rp/attendancesalary/MainActivity.java
@@ -0,0 +1,26 @@
+package com.rp.attendancesalary;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView web = new WebView(this);
+        WebSettings s = web.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setAllowFileAccess(true);
+        s.setAllowContentAccess(false);
+        web.setWebViewClient(new WebViewClient());
+        web.loadUrl("file:///android_asset/index.html");
+        setContentView(web);
+    }
+    @Override public void onBackPressed() {
+        WebView web = (WebView) findViewById(android.R.id.content);
+        super.onBackPressed();
+    }
+}
